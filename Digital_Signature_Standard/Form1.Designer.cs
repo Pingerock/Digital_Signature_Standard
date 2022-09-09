031bcca1225c142c9bbe6a18f5a5c5ae7d2c2b6b
pinge <pinge@PINGEROCK-LAPTP>
2022-09-09 13:21:17 +0300

    Add project files.

---
+++ Digital_Signature_Standard/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace DSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create a signature";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 60);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Verify a signature";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(163, 216);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "E-signature";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Digital_Signature_Standard/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DSS
+{
+    public partial class Form1 : Form
+    {
+
+        public struct SignedMessage
+        {
+            public SignedMessage(byte[] fileBytes, BigInteger r, BigInteger s)
+            {
+                FileBytes = fileBytes;
+                R = r;
+                S = s;
+            }
+            public byte[] FileBytes { get; set; }
+            public BigInteger R { get; set; }
+
+            public BigInteger S { get; set; }
+        }
+
+        public BigInteger Hash1;
+
+        public static BigInteger ExtendedEuclid(BigInteger a, BigInteger b)
+        {
+            bool flag = false;
+            if(a < b)
+            {
+                BigInteger temp = b;
+                b = a;
+                a = temp;
+                flag = true;
+            }
+
+            Vector U = new Vector(a, 1, 0);
+            Vector V = new Vector(b, 0, 1);
+            Vector T = new Vector();
+
+            while(V.v1 != 0)
+            {
+                BigInteger q = U.v1 / V.v1;
+                T.v1 = U.v1 % V.v1;
+                T.v2 = U.v2 - q * V.v2;
+                T.v3 = U.v3 - q * V.v3;
+                U = V;
+                V = T;
+            }
+
+            if (flag) b = a;
+
+            return U.v3 > 0 ? U.v3 : U.v3 + b;
+        }
+
+        public struct Vector
+        {
+            public Vector(BigInteger v1, BigInteger v2, BigInteger v3)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+                this.v3 = v3;
+            }
+
+            public BigInteger v1;
+
+            public BigInteger v2;
+
+            public BigInteger v3;
+        }
+
+        public SignedMessage Message { get; set; }
+        public byte[] FileBytes { get; set; }
+        public static BigInteger p = BigInteger.Parse("89884656743115796742429711405763364460177151692783429800884652449310979263752253529349195459823881715145796498046459238345428121561386626945679753956400077352882071663925459750500807018254028771490434021315691357123734637046894876123496168716251735252662742462099334802433058472377674408598573487858308054417");
+        public static BigInteger q = BigInteger.Parse("1193447034984784682329306571139467195163334221569");
+        public static BigInteger a;
+        public static BigInteger y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Creating a signature
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            BigInteger b = (p - 1) / q;
+
+            BigInteger h = ComputeHash();
+
+            Hash1 = h;
+
+            BigInteger s;
+            BigInteger r;
+
+            do
+            {
+                int g = rand.Next(2, 2000000000);
+                a = BigInteger.ModPow(g, b, p);
+            }
+            while (a == 1);
+
+            int x = rand.Next(1, 2000000000);
+            y = BigInteger.ModPow(a, x, p); 
+
+            do
+            {
+                int k;
+                do
+                {
+                    k = rand.Next(1, int.MaxValue);
+                    r = BigInteger.ModPow(BigInteger.ModPow(a, k, p), 1, q);
+                }
+                while (r == 0); 
+                s = BigInteger.ModPow(ExtendedEuclid(k, q) * (h + x * r), 1, q);
+            }
+            while (s == 0); 
+            Message = new SignedMessage(FileBytes, r, s);
+            button3.Enabled = true;
+            MessageBox.Show("Electronic signature created succesfully.");
+        }
+
+        // Selecting a file
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            FileBytes = File.ReadAllBytes(openFileDialog1.FileName);
+            MessageBox.Show("File loaded succesfully!");
+            button1.Enabled = true;
+        }
+
+        // Verifying a signature
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BigInteger h = ComputeHash();
+            if(Message.R < 0 || Message.R > q || Message.S < 0 || Message.S > q)
+            {
+                MessageBox.Show("Wrong signature!");
+                return;
+            }
+
+            BigInteger sminusone = ExtendedEuclid(Message.S, q);
+            BigInteger u1 = (h * sminusone) % q;
+            BigInteger u2 = (Message.R * sminusone) % q;
+
+            BigInteger v = ((BigInteger.ModPow(a, u1, p) * BigInteger.ModPow(y, u2, p)) % p) % q;
+
+            if (v == Message.R)
+            {
+                MessageBox.Show("Подпись подходит!");
+                if ((Message.S * sminusone) % q == 1)
+                {
+                    MessageBox.Show("True."); 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong signature!");
+            }
+        }
+
+        // Using hash-algorithm SHA-1
+        public BigInteger ComputeHash()
+        {
+            SHA1 sha = SHA1.Create();
+            byte[] hash = sha.ComputeHash(FileBytes);
+            return new BigInteger(hash);
+        }
+
+    }
+}
